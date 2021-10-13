b353b78db8d61518e782dd9483877d8fc4c4f05b
Blake A. Long <blake.a.long@gmail.com>
2021-10-12 20:02:01 -0600

    clean up getLatLong

--- src/utils/getLatLong.js
+++ src/utils/getLatLong.js
@@ -3,10 +3,6 @@ const axios = require('axios')
 const BASE_MB_URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places'
 
 module.exports = getLatLong = async (location) => {
-  // try {
-    const { data } = await axios.get(`${BASE_MB_URL}/${encodeURIComponent(location)}.json?access_token=${process.env.MB_TOKEN}`)
-    return data.features[0]
-  // } catch (err) {
-    throw new Error(err)
-  // }
+  const { data } = await axios.get(`${BASE_MB_URL}/${encodeURIComponent(location)}.json?access_token=${process.env.MB_TOKEN}`)
+  return data.features[0]
 }
