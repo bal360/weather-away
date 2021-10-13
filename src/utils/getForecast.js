3774d73ee56cc18195f2acbbe7aaa11ffbf42888
Blake A. Long <blake.a.long@gmail.com>
2021-10-12 20:10:30 -0600

    clean up getForecast

--- src/utils/getForecast.js
+++ src/utils/getForecast.js
@@ -4,14 +4,10 @@ const getLatLong = require('./getLatLong')
 const BASE_WS_URL = 'http://api.weatherstack.com/'
 
 module.exports = getForecast = async (longLatArray) => {
-  // try {
-    const [long, lat] = longLatArray
-    const latLong = lat + ',' + long
-    
-    const { data } = await axios.get(`${BASE_WS_URL}/forecast?access_key=${process.env.WS_KEY}&query=${latLong}&units=f`)
+  const [long, lat] = longLatArray
+  const latLong = lat + ',' + long
+  
+  const { data } = await axios.get(`${BASE_WS_URL}/forecast?access_key=${process.env.WS_KEY}&query=${latLong}&units=f`)
 
-    return data.current
-  // } catch (err) {
-  //   console.log('Error', err)
-  // }
+  return data.current
 }
